39d6a2557101f0d195c31b22c21a96698c6c14e7
rhakbari <rhakbari.96@gmail.com>
2025-01-04 17:23:29 +0500

    fis

--- api/weekly-activity.js
+++ api/weekly-activity.js
@@ -1,16 +1,16 @@
 // api/weekly-activity.js
-const express = require("express");
-const app = express();
+module.exports = (req, res) => {
+  // Enable CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
-// Enable CORS
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+  // Handle OPTIONS request for CORS
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
 
-app.get("/api/weekly-activity", (req, res) => {
   const randomHeights = [30, 60, 40, 70, 30, 50, 60].map((height) => ({
     deposit: height,
     withdraw: Math.floor(Math.random() * 100),
@@ -41,6 +41,4 @@ app.get("/api/weekly-activity", (req, res) => {
   };
 
   res.json(response);
-});
-
-module.exports = app;
+};
