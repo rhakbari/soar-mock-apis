e6cc8c87aa052c564a1de8387729d6c98c9b7747
rhakbari <rhakbari.96@gmail.com>
2025-01-04 17:11:34 +0500

    added fixes

--- api/mockServer.js
+++ api/mockServer.js
@@ -1,11 +1,6 @@
 const express = require("express");
-const { createServer } = require("http");
-const cors = require("cors");
 
 const app = express();
-const PORT = 80;
-
-app.use(cors());
 
 // Mock API for weekly activity data
 app.get("/weekly-activity", (req, res) => {
@@ -41,20 +36,5 @@ app.get("/weekly-activity", (req, res) => {
   res.json(response);
 });
 
-app.get("/", (req, res) => {
-  const response = {
-    message: "hello world",
-  };
-
-  res.json(response);
-});
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Mock API server running at http://localhost:${PORT}`);
-});
-
-module.exports = (req, res) => {
-  const server = createServer(app);
-  server.emit("request", req, res);
-};
+// Export the app wrapped in a serverless function
+module.exports = app;
